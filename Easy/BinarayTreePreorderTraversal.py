3840e123a22614ab2e582aace396dc72a3ab9a41
Vinh Nguyen <vinh29012005@gmail.com>
2024-09-29 16:18:39 -0500

    Use DFS to Preorder Traverse the Binary Tree

---
+++ Easy/BinarayTreePreorderTraversal.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+from TreeNode import TreeNode
+
+class BinaryTreePreorderTravesal:
+    def preorderTraversal(self, root:Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        
+        stack, ans = [root], []
+
+        while stack:
+            root = stack.pop()
+            if root is not None:
+                ans.append(root)
+                if root.right is not None:
+                    stack.append(root.right)
+                if root.left is not None:
+                    stack.append(root.left)
+        
+        return ans
+    
+    """
+    Space Complexity: O(n)    
+    Time Complexity: O(n) where n is the number of input nodes
+    """
