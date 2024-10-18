328f62ba80e7fa0bc9df774798c0cdfaa5f2e300
Vinh Nguyen <vinh29012005@gmail.com>
2024-10-18 14:37:18 -0500

    my brute force solution to solve removing star from string. I used two stack, first stack to store the reverse given string and second stack to store the result

--- Medium/RemovingStarFromAString/my_solution.py
+++ Medium/RemovingStarFromAString/my_solution.py
@@ -1,12 +1,18 @@
 class my_solution:
     def removeStars(self, s: str) -> str:
         
-        res = ""
-        stack = []
+        stack1 = []
+        stack2 = []
 
-        for i in range(len(s)):
-            stack.append(s[i])
-        
-        
-                
-        return res
+        for i in range(len(s) - 1, -1, -1):
+            stack1.append(s[i])
+
+        while stack1:
+            c = stack1.pop()
+
+            if c != '*':
+                stack2.append(c)
+            else:
+                stack2.pop()
+            
+        return ''.join(stack2)
