8af4a18283319c7ce095bd8aa02f48c01fd5776e
Vinh Nguyen <vinh29012005@gmail.com>
2024-10-24 23:11:36 -0500

    Using recursive Depth First Search to solve the maximum Depth of Binary Tree problem

---
+++ Easy/MaximumDepthOfBinaryTree/RecursiveDFS.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from TreeNode import TreeNode
+
+class RecursiveDFS:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        
+        leftMax = 1 + self.maxDepth(root.left)
+        rightMax = 1 + self.maxDepth(root.right)
+
+        return max(leftMax, rightMax)
